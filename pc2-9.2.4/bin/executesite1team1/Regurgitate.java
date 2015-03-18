9694fb519d281d26e303f2e3d1ef79ac4e120234
Caleb Jasik <caleb@localhost.localdomain>
2015-03-18 13:57:04 -0500

    added pc2

---
+++ pc2-9.2.4/bin/executesite1team1/Regurgitate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class Regurgitate {
+public static void main(String[] args)throws IOException {
+Scanner scanner = new Scanner(System.in);
+System.out.print(scanner.nextLine());
+	}
+}
+
